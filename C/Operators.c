8dd9e797f91a2b773b2816eae12b4b5336a8703a
Rishab Dasari <rishabdasari07official@gmail.com>
2026-08-18 12:22:55 +0530

    Operators

---
+++ C/Operators.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+/*
+
+    int A, B;
+    int Result; 
+    A = 5;
+    B = 6;
+    printf(" %d", A>B);
+    return 0;
+    */
+
+
+    /*
+    int A = 10;
+    printf(" %d", A++ + ++A);
+    */
+
+    /*
+    int A = 6;
+    int B = 5;
+    int C = 4;
+
+    if((A < B) && (B > C))
+    {
+        printf("True \n");
+    }
+    else
+    {
+        printf("False \n");
+    }
+    if((A < B) || (B > C))
+    {
+        printf("True \n");
+    }
+    else
+    {
+        printf("False \n");
+    }
+    return 0;
+    */
+   
+    /*
+    int A = 10;
+    int B = 9;
+    int result = A & B;
+    printf(" %d", result);
+    return 0;
+    */
+}
